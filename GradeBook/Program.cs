0862d866241894091dd8d8818455aa2dade895e5
Socrates Katrivesis <socrateskatrivesis@quickenloans.com>
2020-01-21 16:10:06 -0500

    first commit

---
+++ GradeBook/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char ltrGrade = ' ';
+
+            Console.WriteLine("Welcome to the GradeBook!");
+
+            while (true)
+            {
+                Console.Write("Please enter percentage grade: "); //Makes the cursor on the same line of the console
+
+                string input = Console.ReadLine();
+                double pctGrade = double.Parse(input); //assigns string to pctGrade variable as a double data type
+
+                if (pctGrade >= 90)
+                {
+                    ltrGrade = 'A';
+                }
+                else if (pctGrade >= 80)
+                {
+                    ltrGrade = 'B';
+                }
+                else if (pctGrade >= 70)
+                {
+                    ltrGrade = 'C';
+                }
+                else if (pctGrade >= 60)
+                {
+                    ltrGrade = 'D';
+                }
+                else
+                {
+                    ltrGrade = 'F';
+                }
+
+                Console.WriteLine(pctGrade + " is a(n) " + ltrGrade);
+            }
+        }
+    }
+}
---
+++ Lab2.1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Lab2._1
+{
+    class Program
+    {
+        public static void Sizer(string size, int area)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            string input;
+
+            double length;
+            double width;
+            double height;
+            double area;
+            double perimiter;
+            double volume;
+            bool flag = true;
+
+            Console.WriteLine("Welcome to the room calculator!");
+
+            do
+            {
+                Console.Write("\nPlease enter room Length: ");
+                input = Console.ReadLine();
+                length = double.Parse(input);
+
+                Console.Write("Please enter room Width: ");
+                input = Console.ReadLine();
+                width = double.Parse(input);
+
+                Console.Write("Please enter room Height: ");
+                input = Console.ReadLine();
+                height = double.Parse(input);
+
+                area = CalculateArea(length, width);
+                perimiter = CalculatePerimiter(length, width);
+                volume = CalculateVolume(length, width, height);
+
+                Console.WriteLine($"Area of the room is: {area}");
+                Console.WriteLine($"Perimiter of the room is : {perimiter}");
+                Console.WriteLine($"Volume of the room is: {volume}");
+
+                Sizer(area);
+
+                Console.Write("Would you like to calculate another room? (Please input 1 for yes and 0 for no): ");
+                input = Console.ReadLine();
+
+                if (int.Parse(input) == 0)
+                {
+                    flag = !flag;
+                }
+
+            } while (flag == true);
+
+            Console.WriteLine("Please enjoy your room sizes! Thank you have a great day!!");
+
+        }
+
+        static double CalculateVolume(double length, double width, double height)
+        {
+            return length * width * height;
+        }
+
+        static double CalculatePerimiter(double length, double width)
+        {
+            return (2 * length) + (2 * width);
+        }
+        static double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        private static void Sizer(double area)
+        {
+            string size;
+
+            if (area <= 250)
+            {
+                size = "Small";
+            }
+            else if (area > 250 && area < 650)
+            {
+                size = "Medium";
+            }
+            else
+            {
+                size = "Large";
+            }
+
+            Console.WriteLine($"This is a {size} room.");
+        }
+    }
+}
